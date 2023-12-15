53ae36249509b10d2aebce6e27d07282819c8773
John R <53997057+WolfEye939@users.noreply.github.com>
2023-12-15 07:54:03 -0500

    added home page
    fdagfga

--- credit-card-checker.js
+++ credit-card-checker.js
@@ -1,103 +1,105 @@
-// All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]; // 16 items
+function CCchecker() {
+  // All valid credit card numbers
+  const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
 
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]; // 15 items
+  const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]; // 16 items
 
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+  const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]; // 15 items
 
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+  const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
 
-// All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+  const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+  // All invalid credit card numbers
+  const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+  const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+  const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+  const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+  const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
-// An array of all the arrays above
-const batch = [
-  valid1,
-  valid2,
-  valid3,
-  valid4,
-  valid5,
+  // Can be either valid or invalid
+  const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+  const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+  const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+  const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+  const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
-  invalid1,
-  invalid2,
-  invalid3,
-  invalid4,
-  invalid5,
+  // An array of all the arrays above
+  const batch = [
+    valid1,
+    valid2,
+    valid3,
+    valid4,
+    valid5,
 
-  mystery1,
-  mystery2,
-  mystery3,
-  mystery4,
-  mystery5,
-];
+    invalid1,
+    invalid2,
+    invalid3,
+    invalid4,
+    invalid5,
 
-//=============================================================================================
-//                                   QUICK NOTE ON MY VISUAL SYNTAX
-//  for if else statements I use 2 spaces on the else statements since they are directly
-//  Related to the if statement. Also if a function contains 1 action/property I put it 
-//  on one line, this is excluding actions such as continue, break or other similar 
-//  functions, I like to double indent them so they stand out visually.
-//
-//  This style is based off of Valve's source language for QC, VMT, FGD, and "gameinfo.txt" 
-//=============================================================================================
+    mystery1,
+    mystery2,
+    mystery3,
+    mystery4,
+    mystery5,
+  ];
 
-const validateCred = (arr) => {
-  let sum = 0;
+  //=============================================================================================
+  //                                   QUICK NOTE ON MY VISUAL SYNTAX
+  //  for if else statements I use 2 spaces on the else statements since they are directly
+  //  Related to the if statement. Also if a function contains 1 action/property I put it 
+  //  on one line, this is excluding actions such as continue, break or other similar 
+  //  functions, I like to double indent them so they stand out visually.
+  //
+  //  This style is based off of Valve's source language for QC, VMT, FGD, and "gameinfo.txt" 
+  //=============================================================================================
 
-  for (let i = arr.length - 1; i >= 0; i--) {
-      let digit = arr[i];
-      let position = arr.length - i;
-      if (position % 2 === 0) {
-          digit = digit * 2;
-          if (digit > 9) digit -= 9;
-      }
-      sum += digit;
+  const validateCred = (arr) => {
+    let sum = 0;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        let digit = arr[i];
+        let position = arr.length - i;
+        if (position % 2 === 0) {
+            digit = digit * 2;
+            if (digit > 9) digit -= 9;
+        }
+        sum += digit;
+    }
+
+    return sum % 10 === 0;
   }
 
-  return sum % 10 === 0;
+  const findInvalidCards = (nestedArrays) => {
+  return nestedArrays.filter((arr) => !validateCred(arr));
+  };
+
+  console.log(findInvalidCards(batch));
+
+  const idInvalidCardCompanies = (arr) => {
+    let companiesArr = [];
+
+    arr.forEach( (subArr) => {
+        if (subArr[0] === 3) {
+            if (!companiesArr.includes("Amex")) companiesArr.push("Amex");
+        } else if (subArr[0] === 4) {
+            if (!companiesArr.includes("Visa")) companiesArr.push("Visa");
+        } else if (subArr[0] === 5) {
+            if (!companiesArr.includes("Mastercard")) companiesArr.push("Mastercard");
+        } else if (subArr[0] === 6) {
+            if (!companiesArr.includes("Discover")) companiesArr.push("Discover");
+        } else {
+            console.log('Company not found');
+        }
+    });
+
+    return companiesArr;
+  };
+
+  console.log(idInvalidCardCompanies(findInvalidCards(batch)));
 }
-
-const findInvalidCards = (nestedArrays) => {
-return nestedArrays.filter((arr) => !validateCred(arr));
-};
-
-console.log(findInvalidCards(batch));
-
-const idInvalidCardCompanies = (arr) => {
-  let companiesArr = [];
-
-  arr.forEach( (subArr) => {
-      if (subArr[0] === 3) {
-          if (!companiesArr.includes("Amex")) companiesArr.push("Amex");
-      } else if (subArr[0] === 4) {
-          if (!companiesArr.includes("Visa")) companiesArr.push("Visa");
-      } else if (subArr[0] === 5) {
-          if (!companiesArr.includes("Mastercard")) companiesArr.push("Mastercard");
-      } else if (subArr[0] === 6) {
-          if (!companiesArr.includes("Discover")) companiesArr.push("Discover");
-      } else {
-          console.log('Company not found');
-      }
-  });
-
-  return companiesArr;
-};
-
-console.log(idInvalidCardCompanies(findInvalidCards(batch)));
-
 /*    OLDER CODE LEFT FOR REFERENCE/ARCHIVAL PURPOSE
 
 
--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,66 +1,67 @@
 
+function RPS() {
+  const getUserChoice = userInput => {
+      userInput = userInput.toLowerCase();    // sets wording to lowercase
 
-const getUserChoice = userInput => {
-    userInput = userInput.toLowerCase();    // sets wording to lowercase
+      if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors')      // checks if user input is rock paper scissors, and returns one of those values
+          { return userInput; }                   
+        else { console.log('invalid input'); }                                            // and logs an error if the user typed anything else
+  }
 
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors')      // checks if user input is rock paper scissors, and returns one of those values
-        { return userInput; }                   
-      else { console.log('invalid input'); }                                            // and logs an error if the user typed anything else
+  function GUC_test() {                           // GUC: (g)et(U)ser(C)hoice
+    console.log( getUserChoice('Paper') );        // Should print 'paper'
+    console.log( getUserChoice('fork') );         // should write 'invalid input'
+  } 
+  //GUC_test();
+
+  function getComputerChoice() {
+      const Cnum = Math.floor(Math.random() * 3);
+
+      switch (Cnum) {
+        case 0:
+          return 'rock';
+          break;
+        case 1:
+          return 'paper';
+          break;
+        case 2:
+          return 'scissors';
+          break;
+      }
+  }
+
+  function GCC_test() {
+    console.log( getComputerChoice() );
+    console.log( getComputerChoice() );
+    console.log( getComputerChoice() );
+  }
+  //GCC_test();     // GCC: (G)et(C)omputer(C)hoice
+
+
+  function determineWinner(userChoice, computerChoice) {
+      if (userChoice === computerChoice) 
+        { return 'The game is a tie!'; }
+
+      if ( userChoice === 'paper') {
+          if (computerChoice === 'scissors')
+            { return 'The computer won!'; }
+          else { return 'You won!'; }
+      }
+
+      if (userChoice === 'scissors') {
+          if (computerChoice === 'rock')
+            { return 'The computer won!'; }
+          else { return 'You won!'; }
+      }
+  }
+
+  const playGame = () => {
+    const userChoice =        getUserChoice('scissors');
+    const computerChoice =    getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The infernal machine counters with: ' + computerChoice);
+    console.log( determineWinner(userChoice, computerChoice) );
+  };
+
+  playGame();
 }
-
-function GUC_test() {                           // GUC: (g)et(U)ser(C)hoice
-  console.log( getUserChoice('Paper') );        // Should print 'paper'
-  console.log( getUserChoice('fork') );         // should write 'invalid input'
-} 
-//GUC_test();
-
-function getComputerChoice() {
-    const Cnum = Math.floor(Math.random() * 3);
-
-    switch (Cnum) {
-      case 0:
-        return 'rock';
-        break;
-      case 1:
-        return 'paper';
-        break;
-      case 2:
-        return 'scissors';
-        break;
-    }
-}
-
-function GCC_test() {
-  console.log( getComputerChoice() );
-  console.log( getComputerChoice() );
-  console.log( getComputerChoice() );
-}
-//GCC_test();     // GCC: (G)et(C)omputer(C)hoice
-
-
-function determineWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) 
-      { return 'The game is a tie!'; }
-
-    if ( userChoice === 'paper') {
-        if (computerChoice === 'scissors')
-          { return 'The computer won!'; }
-        else { return 'You won!'; }
-    }
-
-    if (userChoice === 'scissors') {
-        if (computerChoice === 'rock')
-          { return 'The computer won!'; }
-        else { return 'You won!'; }
-    }
-}
-
-const playGame = () => {
-  const userChoice =        getUserChoice('scissors');
-  const computerChoice =    getComputerChoice();
-  console.log('You threw: ' + userChoice);
-  console.log('The infernal machine counters with: ' + computerChoice);
-  console.log( determineWinner(userChoice, computerChoice) );
-};
-
-playGame();
